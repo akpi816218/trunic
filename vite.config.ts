fc9431d0dde944c1e5ff68c5098a5c621e5e7167
akpi816218 <gh@akpi.is-a.dev>
2024-03-28 18:42:24 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/',
+	base: '/trunic/',
 	root: 'src',
 	appType: 'mpa',
 	build: {
